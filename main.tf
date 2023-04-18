a2b24e3326b88066b26ec899885d9ebcaf459acd
bigpil <nathanlmeehan@gmail.com>
2023-04-17 23:41:43 -0400

    modify main

--- main.tf
+++ main.tf
@@ -1,16 +1,30 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+      aws = {
+        source  = "hashicorp/aws"
+        version = "~> 4.0"
+      }
+    }
+  
+  backend "remote" {
+  # The name of your Terraform Cloud organization.
+  organization = "sdn-l3"
+
+  # The name of the Terraform Cloud workspace to store Terraform state files in.
+  workspaces {
+    name = "aws-tf-cloud-1"
     }
   }
 }
+
+  
 
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
+ 
+
 
 data "aws_ami" "amazon_linux" {
 	most_recent = true
